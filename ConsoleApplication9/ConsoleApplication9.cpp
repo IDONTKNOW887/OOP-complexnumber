ae5470e651d66e052bc0c00c47f0fd4e2d81064a
Abdulla Ashurov <ashurovabdulla24@gmail.com>
2022-03-09 16:56:00 +0500

    Вывод произведение

--- ConsoleApplication9/ConsoleApplication9.cpp
+++ ConsoleApplication9/ConsoleApplication9.cpp
@@ -1,8 +1,21 @@
 ﻿#include <iostream>
 using namespace std;
 
+struct Drob
+{
+	int chis;
+	int znam;
+	void output()
+	{
+		cout << chis * znam;
+	}
+};
+
+
 int main()
 {
 	setlocale(LC_ALL, "rus");
 	srand(time(NULL));
+	Drob a{2, 3};
+
 }
