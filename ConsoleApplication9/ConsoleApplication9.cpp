b1f37397dcdfe09a3ebd1bd4690d0ac01370405d
Abdulla Ashurov <ashurovabdulla24@gmail.com>
2022-03-14 18:46:38 +0500

    переделать функции

--- ConsoleApplication9/ConsoleApplication9.cpp
+++ ConsoleApplication9/ConsoleApplication9.cpp
@@ -1,4 +1,5 @@
 ﻿#include <iostream>
+#include <fstream>
 using namespace std;
 
 class Complex
@@ -11,33 +12,47 @@ public:
 		vesh = a; 
 		mnim = b; 
 	}
-
-	void input(Complex a, Complex b)
-	{
-		cout << "Введите первое число: ";
-		cin >> a;
-		cout << "Введите второе число: ";
-		
-	}
 	
-	void sum(Complex a, Complex b)
+	float Vesh() { return vesh; }
+	int Mnim() { return mnim; }
+
+	void sum(Complex b)
 	{
-		cout << a.vesh + b.vesh << " + " << a.mnim + b.mnim <<" i";
+		cout << vesh + b.Vesh() << " + " << mnim + b.Mnim() <<"i" << endl;
+		float s1 = vesh + b.Vesh();
+		int s2 = mnim + b.Mnim();
 	}
 
-	void min(Complex a, Complex b)
+	void min(Complex b)
 	{
-		cout << a.vesh - b.vesh << " + " <<  a.mnim - b.mnim << " i";
+		float m1;
+		int m2;
+		if (mnim - b.Mnim() > 0)
+		{
+			cout << vesh - b.Vesh() << " + " << mnim - b.Mnim() << "i" << endl;
+			float m1 = vesh - b.Vesh();
+			int m2 = mnim - b.Mnim();
+		}
+		else
+		{
+			cout << vesh - b.Vesh() << " " << mnim - b.Mnim() << "i" << endl;
+			float m1 = vesh - b.Vesh();
+			int m2 = mnim - b.Mnim();
+		}
 	}
 
-	void umn(Complex a, Complex b)
+	void umn(Complex b)
 	{
-		cout << a.vesh * b.vesh - a.mnim * b.mnim << " + " << a.mnim * b.vesh - b.mnim * a.vesh << " i";
+		float u1;
+		int u2;
+		cout << vesh * b.Vesh() - mnim * b.Mnim() << " + " << mnim * b.Vesh() + b.mnim * vesh << "i" << endl;
+		float u1 = vesh * b.Vesh();
+		int u2 = mnim - b.Mnim();
 	}
 
-	void del(Complex a, Complex b)
+	void del(Complex b)
 	{
-		cout << (a.vesh * b.vesh + a.mnim * b.mnim) / (b.vesh * b.vesh + b.mnim * b.mnim) << (b.mnim * a.vesh - a.vesh * b.mnim) / (b.vesh * b.vesh + b.mnim * b.mnim);
+		cout << (vesh * b.Vesh() + mnim * b.Mnim()) / (b.Vesh() * b.Vesh() + b.Mnim() * b.Mnim()) << (b.Mnim() * vesh - vesh * b.Mnim()) / (b.Vesh() * b.Vesh() + b.Mnim() * b.Mnim());
 	}
 };
 
@@ -46,7 +61,22 @@ int main()
 {
 	setlocale(LC_ALL, "rus");
 	srand(time(NULL));
-	float a;
-	int b;
-	sum();
+	float a, c;
+	int b, d;
+	cout << "Введите первое число: ";
+	cin >> a;
+	cout << "Введите второе число: ";
+	cin >> b;
+	cout << "Введите первое число: ";
+	cin >> c;
+	cout << "Введите второе число: ";
+	cin >> d;
+	Complex nume(a, b);
+	Complex nume2(c, d);
+	ofstream fout("data_types.txt", ios_base::out | ios_base::trunc);
+	nume.sum(nume2);
+	nume.min(nume2);
+	nume.umn(nume2);
+	nume.del(nume2);
+
 }
